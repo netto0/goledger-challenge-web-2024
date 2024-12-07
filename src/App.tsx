529b169f7dd1baa3e426f67140b49fbf066460aa
Netto <orlandiomnetto@gmail.com>
2024-12-07 18:12:19 -0300

    - Criado componente de sugestão de playlists

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { AssetListsContext } from "./providers/assetLists";
 import SearchBar from "./components/SearchBar";
 import PagesMenu from "./components/PagesMenu";
+import PlaylistsPreview from "./components/PlaylistsPreview";
 
 function App() {
   const {getArtists, getSongs, getAlbums, getPlaylists} = React.useContext(AssetListsContext)
@@ -12,19 +13,14 @@ function App() {
   return (
     <div id="masterDiv" className="grid grid-cols-[1fr_3fr_1.3fr] gap-0.5 h-screen text-[#EBEBEB]">
       <div id="leftBar" className="bg-red-500 flex flex-col w-full h-full ">
-        <div id="searchBarDiv" className="border flex border-gray-700 w-full h-[10%] px-5 py-2 items-center justify-center">
+        <div id="searchBarDiv" className="border flex border-gray-700 w-full min-h-[10%] px-5 py-2 items-center justify-center">
           <SearchBar />
         </div>
-        <div id="menuDiv" className="border flex flex-col border-gray-700 w-full h-fit px-5 py-2">
+        <div id="menuDiv" className="border flex flex-col border-gray-700 w-full h-[30%] px-5 py-2">
           <PagesMenu />
         </div>
         <div id="playlistsDiv" className="border flex flex-col border-gray-700 w-full flex-grow px-5 py-2">
-          <strong>Playlists</strong>
-          <ul>
-            <li>Playlist</li>
-            <li>Playlist</li>
-            <li>Playlist</li>
-          </ul>
+          <PlaylistsPreview />
         </div>
       </div>
       <div id="centerDiv" className="bg-green-500 flex flex-col w-full h-full">
--- src/components/PagesMenu.tsx
+++ src/components/PagesMenu.tsx
@@ -5,8 +5,8 @@ import { IoIosAlbums } from "react-icons/io";
 export default function PagesMenu() {
   return (
     <div className="">
-      <h2 className="font-bold text-4xl my-4">Menu</h2>
-      <ul className="flex flex-col gap-5 pl-2">
+      <h2 className="font-bold text-3xl my-4">Menu</h2>
+      <ul className="flex flex-col gap-5 pl-1">
         <li><a href="" className="flex items-center text-3xl gap-2 hover:text-gray-300 hover:scale-105 hover:translate-x-3 transition-all"><BsPersonFill />Artistas</a></li>
         <li><a href="" className="flex items-center text-3xl gap-2 hover:text-gray-300 hover:scale-105 hover:translate-x-3 transition-all"><PiMusicNotesFill />Músicas</a></li>
         <li><a href="" className="flex items-center text-3xl gap-2 hover:text-gray-300 hover:scale-105 hover:translate-x-3 transition-all"><IoIosAlbums />Álbums</a></li>
