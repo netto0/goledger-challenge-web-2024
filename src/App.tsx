6af02a2295425a20fa2144b8f1cba03b9f54333c
Netto <orlandiomnetto@gmail.com>
2024-12-07 07:20:50 -0300

    - Preparado o esqueleto da janela principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { AssetListsContext } from "./providers/assetLists";
-import Tests from "./components/Tests";
 
 function App() {
   const {getArtists, getSongs, getAlbums, getPlaylists} = React.useContext(AssetListsContext)
@@ -9,12 +8,54 @@ function App() {
   useEffect(() => {getArtists(), getSongs(), getAlbums(), getPlaylists()},[])
 
   return (
-    <>
-      <Tests />
-    </>
+    <div id="masterDiv" className="grid grid-cols-[1fr_3fr_1.3fr] gap-0.5 h-screen">
+      <div id="leftBar" className="bg-red-500 flex flex-col w-full h-full ">
+        <div id="searchBarDiv" className="border flex border-gray-700 w-full h-[10%] p-2 items-center justify-center">SEARCH BAR</div>
+        <div id="menuDiv" className="border flex flex-col border-gray-700 w-full h-[30%] p-2">
+          <strong>Menu</strong>
+          <ul>
+            <li>Artistas</li>
+            <li>Músicas</li>
+            <li>Álbums</li>
+          </ul>
+        </div>
+        <div id="playlistsDiv" className="border flex flex-col border-gray-700 w-full h-[60%] p-2">
+          <strong>Playlists</strong>
+          <ul>
+            <li>Playlist</li>
+            <li>Playlist</li>
+            <li>Playlist</li>
+          </ul>
+        </div>
+      </div>
+      <div id="centerDiv" className="bg-green-500 flex flex-col w-full h-full">
+        <div id="titleDiv" className="border flex flex-col border-gray-700 w-full h-[10%] p-2 justify-center">NOME DO ARTISTA</div>
+        <div id="mainContentDiv" className="border flex flex-col border-gray-700 w-full h-[90%] p-2">
+          <strong>Músicas</strong>
+          <ul>
+            <li>Musica</li>
+            <li>Musica</li>
+            <li>Musica</li>
+          </ul>
+        </div>
+      </div>
+      <div id="rightBar" className="bg-blue-500 w-full h-full">
+        <div id="userInfosDiv" className="border flex flex-col border-gray-700 w-full h-[10%] p-2 justify-center">foto USUÁRIO</div>
+        <div id="sugestedArtistsDiv" className="border flex flex-col border-gray-700 w-full h-[90%] p-2">
+          <strong>Artistas sugeridos</strong>
+          <ul>
+            <li>Artista</li>
+            <li>Artista</li>
+            <li>Artista</li>
+            <li>Artista</li>
+            <li>Artista</li>
+            <li>Artista</li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default App;
-
-// [{"@key": "song:21fb21f9-bca2-5734-9c7d-9a9b3d989cd7"}, {"@key": "song:229816cc-8f4c-51c1-9c0f-2375c6d51375"}, {"@key": "song:2545d178-4ab1-582a-a93f-b049fe279ff0"}]
